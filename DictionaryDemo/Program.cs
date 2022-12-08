08a372ca6564acbdb6ecc9ebe13396a434ae9b6e
siddharth gandhi <siddharthgandhi@outlook.com>
2022-12-08 09:02:11 +0530

    [Siddharth]Add: Dictionary Demo

--- DictionaryDemo/Program.cs
+++ DictionaryDemo/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            doDictionaryDemo();
+        }
+        private static void doDictionaryDemo()
+        {
+           Console.WriteLine("\n doDictionaryDemo");
+            Dictionary<int,string> dictionary = new Dictionary<int,string>();
+            dictionary.Add(100, "Amit");
+            dictionary.Add(101, "Rahul");
+            dictionary.Add(102, "Vijay");
+            Console.WriteLine("Access value using key(key=101):" + dictionary[101]);
+            Console.WriteLine("\n Iterating Dictionary:");
+
+            foreach(var element in dictionary)
+            {
+                Console.WriteLine("Key= " +element.Key+" & Value =" + element.Value);
+            }
+        }
+    }
+}
